5e4bc0fccead20e007c02718550dbdb2483e0fa0
Muhammad Zaky Sarbin <zaky15275@gmail.com>
2026-08-10 16:38:28 +0800

    API

--- api_get_data.php
+++ api_get_data.php
@@ -132,80 +132,78 @@ if ($device_id === 'indoor') {
             }
         }
 
-        if ($is_online) {
-            $apiValue = isset($row['api']) ? (float)$row['api'] : 0;
-            $rawAsap = isset($row['asap']) ? $row['asap'] : 0;
-            
-            $strApi = isset($row['api']) ? trim(strtolower((string)$row['api'])) : '';
-            if ($strApi === 'terdeteksi api' || $strApi === 'dekat' || $strApi === 'sedang' || $strApi === 'tinggi' || $apiValue > 0.5) {
-                $apiStatus = "Terdeteksi Api";
-            } else {
-                $apiStatus = "Aman";
-            }
-            
-            if (is_numeric($rawAsap)) {
-                $fAsap = (float)$rawAsap;
-                if ($fAsap > ($fAsap > 1 ? 750 : 0.5)) {
-                    $asapStatus = "Tinggi";
-                } else if ($fAsap > ($fAsap > 1 ? 350 : 0.25)) {
-                    $asapStatus = "Sedang";
-                } else {
-                    $asapStatus = "Normal";
-                }
-                $asapNum = $fAsap;
-            } else {
-                $strAsap = trim((string)$rawAsap);
-                if (strcasecmp($strAsap, 'Tinggi') === 0 || strcasecmp($strAsap, 'Bahaya') === 0) {
-                    $asapStatus = "Tinggi";
-                    $asapNum = 1;
-                } else if (strcasecmp($strAsap, 'Sedang') === 0 || strcasecmp($strAsap, 'Waspada') === 0) {
-                    $asapStatus = "Sedang";
-                    $asapNum = 0.5;
-                } else {
-                    $asapStatus = "Normal";
-                    $asapNum = 0;
-                }
-            }
-            
-            $suhuVal = isset($row['suhu']) ? (float)$row['suhu'] : 0.0;
-            $kelembapanVal = isset($row['kelembapan']) ? (float)$row['kelembapan'] : 0.0;
-            $teganganVal = isset($row['tegangan']) ? (float)$row['tegangan'] : 0.0;
-            $arusVal = isset($row['arus']) ? (float)$row['arus'] : 0.0;
-
-            $isDanger = ($apiStatus === "Terdeteksi Api" || $asapStatus === "Tinggi" 
-                || ($suhuVal > $limit_suhu) 
-                || ($kelembapanVal > $limit_kelembapan) 
-                || ($teganganVal > $limit_tegangan) 
-                || ($arusVal > $limit_arus));
-
-            echo json_encode([
-                "error"            => false,
-                "waktu"            => date('H:i:s', strtotime($waktu_raw)),
-                "api"              => $apiStatus,
-                "asap"             => $asapStatus,
-                "asap_value"       => $asapNum,
-                "suhu"             => number_format($suhuVal, 1),
-                "kelembapan"       => number_format($kelembapanVal, 1),
-                "tegangan"         => number_format($teganganVal, 1),
-                "arus"             => number_format($arusVal, 2),
-                "rssi"             => $row['rssi'] ?? '-',
-                "ip"               => !empty($row['ip_address']) ? $row['ip_address'] : '-',
-                "latitude"         => isset($row['latitude']) ? (float)$row['latitude'] : null,
-                "longitude"        => isset($row['longitude']) ? (float)$row['longitude'] : null,
-                "isDanger"         => $isDanger,
-                "apiValue"         => ($apiStatus === "Terdeteksi Api") ? 1 : 0,
-                "status"           => "Online",
-                "limit_suhu"       => $limit_suhu,
-                "limit_kelembapan" => $limit_kelembapan,
-                "limit_tegangan"   => $limit_tegangan,
-                "limit_arus"       => $limit_arus,
-                "batas_suhu"       => $limit_suhu,
-                "batas_kelembapan" => $limit_kelembapan,
-                "batas_tegangan"   => $limit_tegangan,
-                "batas_arus"       => $limit_arus
-            ]);
-            exit();
+        $apiValue = isset($row['api']) ? (float)$row['api'] : 0;
+        $rawAsap = isset($row['asap']) ? $row['asap'] : 0;
+        
+        $strApi = isset($row['api']) ? trim(strtolower((string)$row['api'])) : '';
+        if ($strApi === 'terdeteksi api' || $strApi === 'dekat' || $strApi === 'sedang' || $strApi === 'tinggi' || $apiValue > 0.5) {
+            $apiStatus = "Terdeteksi Api";
+        } else {
+            $apiStatus = "Aman";
         }
+        
+        if (is_numeric($rawAsap)) {
+            $fAsap = (float)$rawAsap;
+            if ($fAsap > ($fAsap > 1 ? 750 : 0.5)) {
+                $asapStatus = "Tinggi";
+            } else if ($fAsap > ($fAsap > 1 ? 350 : 0.25)) {
+                $asapStatus = "Sedang";
+            } else {
+                $asapStatus = "Normal";
+            }
+            $asapNum = $fAsap;
+        } else {
+            $strAsap = trim((string)$rawAsap);
+            if (strcasecmp($strAsap, 'Tinggi') === 0 || strcasecmp($strAsap, 'Bahaya') === 0) {
+                $asapStatus = "Tinggi";
+                $asapNum = 1;
+            } else if (strcasecmp($strAsap, 'Sedang') === 0 || strcasecmp($strAsap, 'Waspada') === 0) {
+                $asapStatus = "Sedang";
+                $asapNum = 0.5;
+            } else {
+                $asapStatus = "Normal";
+                $asapNum = 0;
+            }
+        }
+        
+        $suhuVal = isset($row['suhu']) ? (float)$row['suhu'] : 0.0;
+        $kelembapanVal = isset($row['kelembapan']) ? (float)$row['kelembapan'] : 0.0;
+        $teganganVal = isset($row['tegangan']) ? (float)$row['tegangan'] : 0.0;
+        $arusVal = isset($row['arus']) ? (float)$row['arus'] : 0.0;
+
+        $isDanger = ($apiStatus === "Terdeteksi Api" || $asapStatus === "Tinggi" 
+            || ($suhuVal > $limit_suhu) 
+            || ($kelembapanVal > $limit_kelembapan) 
+            || ($teganganVal > $limit_tegangan) 
+            || ($arusVal > $limit_arus));
+
+        echo json_encode([
+            "error"            => false,
+            "waktu"            => date('H:i:s', strtotime($waktu_raw)),
+            "api"              => $apiStatus,
+            "asap"             => $asapStatus,
+            "asap_value"       => $asapNum,
+            "suhu"             => number_format($suhuVal, 1),
+            "kelembapan"       => number_format($kelembapanVal, 1),
+            "tegangan"         => number_format($teganganVal, 1),
+            "arus"             => number_format($arusVal, 2),
+            "rssi"             => $row['rssi'] ?? '-',
+            "ip"               => !empty($row['ip_address']) ? $row['ip_address'] : '-',
+            "latitude"         => isset($row['latitude']) ? (float)$row['latitude'] : null,
+            "longitude"        => isset($row['longitude']) ? (float)$row['longitude'] : null,
+            "isDanger"         => $isDanger,
+            "apiValue"         => ($apiStatus === "Terdeteksi Api") ? 1 : 0,
+            "status"           => $is_online ? "Online" : "Offline",
+            "limit_suhu"       => $limit_suhu,
+            "limit_kelembapan" => $limit_kelembapan,
+            "limit_tegangan"   => $limit_tegangan,
+            "limit_arus"       => $limit_arus,
+            "batas_suhu"       => $limit_suhu,
+            "batas_kelembapan" => $limit_kelembapan,
+            "batas_tegangan"   => $limit_tegangan,
+            "batas_arus"       => $limit_arus
+        ]);
+        exit();
     }
 
     echo json_encode([
